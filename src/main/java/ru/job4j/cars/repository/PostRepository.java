89f57a7cd246885e30ad14252f4a71e2e7b489c1
Oywayten <oywayten@gmail.com>
2023-04-25 10:48:15 +0300

    refactor PostRepository's methods

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -2,13 +2,13 @@ package ru.job4j.cars.repository;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Oywayten 01.04.2023.
@@ -18,63 +18,27 @@ import java.util.List;
 @AllArgsConstructor
 public class PostRepository {
 
-    private final SessionFactory sf;
+    private static final String GET_ALL = "FROM Post p";
+    private static final String LAST_DAY = "WHERE p.created >= :date";
+    private static final String HAS_PHOTO = "WHERE p.photo IS NOT NULL";
+    private static final String BY_BRAND = "JOIN p.car c WHERE c.name = :brand%%";
+    private static final String ORDER_ASC_BY_CREATED = "ORDER BY p.created ASC";
+    private static final String GET_ALL_FROM_LAST_DAY_ASC = String.format("%s %s %s", GET_ALL, LAST_DAY, ORDER_ASC_BY_CREATED);
+    private static final String GET_ALL_WITH_PHOTO_ASC = String.format("%s %s %s", GET_ALL, HAS_PHOTO, ORDER_ASC_BY_CREATED);
+    private static final String GET_ALL_BY_BRAND_ASC = String.format("%s %s %s", GET_ALL, BY_BRAND, ORDER_ASC_BY_CREATED);
+    private final CrudRepository crudRepository;
 
-    public List<Post> getAllFromToday() {
-        Session session = sf.openSession();
-        Transaction transaction = null;
-        List<Post> postList = List.of();
-        try {
-            transaction = session.beginTransaction();
-            postList = session.createQuery("from Post WHERE created = current_date", Post.class).getResultList();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            log.info(e.getMessage());
-        } finally {
-            session.close();
-        }
-        return postList;
+    public List<Post> getAllFromLastDay() {
+        return crudRepository
+                .query(GET_ALL_FROM_LAST_DAY_ASC,
+                        Post.class, Map.of("date", LocalDateTime.now().minusDays(1).truncatedTo(ChronoUnit.SECONDS)));
     }
 
     public List<Post> getAllWithPhoto() {
-        Session session = sf.openSession();
-        Transaction transaction = null;
-        List<Post> postList = List.of();
-        try {
-            transaction = session.beginTransaction();
-            postList = session.createQuery("from Post WHERE photo IS NOT NULL", Post.class).getResultList();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            log.info(e.getMessage());
-        } finally {
-            session.close();
-        }
-        return postList;
+        return crudRepository.query(GET_ALL_WITH_PHOTO_ASC, Post.class);
     }
 
-    public List<Post> getAllByMake(String make) {
-        Session session = sf.openSession();
-        Transaction transaction = null;
-        List<Post> postList = List.of();
-        try {
-            transaction = session.beginTransaction();
-            postList = session.createQuery(String.format("from Post p join p.car c WHERE c.name like %s%%", make), Post.class).getResultList();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            log.info(e.getMessage());
-        } finally {
-            session.close();
-        }
-        return postList;
+    public List<Post> getAllByBrand(String brand) {
+        return crudRepository.query(GET_ALL_BY_BRAND_ASC, Post.class, Map.of("brand", brand));
     }
-
 }
